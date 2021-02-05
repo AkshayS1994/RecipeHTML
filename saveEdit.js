7e0cc883e5415dad95b56f65c8adb206885f5410
Akshay S <akshay@acism.com>
2021-02-05 18:33:36 +0530

    Editable Name of Recipe

--- saveEdit.js
+++ saveEdit.js
@@ -1,22 +1,36 @@
 function saveEdits() {
-
-    //get the editable element
-    var editElem = document.getElementById("edit");
-    
-    //get the edited element content
-    var userVersion = editElem.innerHTML;
-    
-    //save the content to local storage
-    localStorage.userEdits = userVersion;
-    
-    //write a confirmation to the user
-    document.getElementById("update").innerHTML="Edits saved!";
-    
-    }
+    var editedElement = document.getElementById("editTitle"); //get the editable element
+    var userVersion = editedElement.innerHTML; //get the edited element content
+    localStorage.userEdit = userVersion; //save the content to local storage
+}
 
 function checkEdits() {
 
-        //find out if the user has previously saved edits
-        if(localStorage.userEdits!=null)
-        document.getElementById("edit").innerHTML = localStorage.userEdits;
-        }
+    //find out if the user has previously saved edits
+    if (localStorage.userEdit != null)
+        document.getElementById("editTitle").innerHTML = localStorage.userEdit;
+}
+
+$(document).ready(function() {
+    var id = '#dialog';
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+    $('#mask').css({ 'width': maskWidth, 'height': maskHeight });
+    $('#mask').fadeIn(500);
+    $('#mask').fadeTo("slow", 0.9);
+    var winH = $(window).height();
+    var winW = $(window).width();
+    $(id).css('top', winH / 2 - $(id).height() / 2);
+    $(id).css('left', winW / 2 - $(id).width() / 2);
+    $(id).fadeIn(2000);
+    $('.window .close').click(function(e) {
+        e.preventDefault();
+        $('#mask').hide();
+        $('.window').hide();
+    });
+    $('#mask').click(function() {
+        $(this).hide();
+        $('.window').hide();
+    });
+
+});
